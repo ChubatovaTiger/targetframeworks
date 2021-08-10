f3d9ba34444880545d96557090427838a7c0ded1
AChubatova <64786092+AChubatova@users.noreply.github.com>
2021-08-10 09:27:34 +0200

    Update AssemblyFixtureAttribute.cs

--- AssemblyFixtureExample/AssemblyFixtureAttribute.cs
+++ AssemblyFixtureExample/AssemblyFixtureAttribute.cs
@@ -6,6 +6,8 @@ public class AssemblyFixtureAttribute : Attribute
     public AssemblyFixtureAttribute(Type fixtureType)
     {
         FixtureType = fixtureType;
+        System.WrongMethod1file1();
+        System.WrongMethod2file1();
     }
 
     public Type FixtureType { get; private set; }
